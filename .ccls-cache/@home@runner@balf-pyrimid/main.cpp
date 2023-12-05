6e9d075380dde1e11f908ca2fd0bb470c1362e13
np7032 <104293378+np7032@users.noreply.github.com>
2023-12-05 09:42:34 +0000

    Initial commit

---
+++ .ccls-cache/@home@runner@balf-pyrimid/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace  std;
+
+int main() {
+  int n=5;
+  for(int i=0;i<5;i=i+1)
+    {
+      for(int j=0;j<i+1;j=j+1)
+      {
+        cout<<"*";
+        
+      }
+
+     cout<< endl;
+        
+    }
+}
